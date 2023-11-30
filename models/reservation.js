470794f8e2532e6fe00aeb8dc954632443707bc6
alocke8181 <alocke_8181@yahoo.com>
2023-11-30 16:27:43 -0500

    added save() for reservations

--- models/reservation.js
+++ models/reservation.js
@@ -38,6 +38,23 @@ class Reservation {
 
     return results.rows.map(row => new Reservation(row));
   }
+
+  static async save(){
+    if (this.id === undefined){
+      const result = await db.query(`
+        INSERT INTO reservations (customer_id, start_at, num_guests, notes)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id`,
+        [this.customerId, this.numGuests, this.startAt, this.notes]);
+        this.id = results.rows[0].id;
+    } else {
+      await db.query(`
+        UPDATE customers SET customer_id = $1, start_at = $2, num_guests = $3, notes = $4
+        WHERE id = $5`,
+        [this.customerId, this.numGuests, this.startAt, this.notes, this.id]);
+    }
+  }
+
 }
 
 
